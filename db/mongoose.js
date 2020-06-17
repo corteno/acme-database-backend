1bf540501cbe28f22360315cd84e627f1aaf937c
Dávid Borsodi <corten.mgs@gmail.com>
2020-06-17 15:10:20 +0200

    - Added disclaimer

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = require('promise');
+// Yes I know this is bad practice and I should use .env or something like that
+// to store credentials but this is only for show
 mongoose.connect(
     'mongodb://acme-admin:acme-admin12@ds215633.mlab.com:15633/acme-database', {
         useNewUrlParser: true,
